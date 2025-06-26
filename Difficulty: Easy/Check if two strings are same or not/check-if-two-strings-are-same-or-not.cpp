93476c8e9247f67cf2d4ad337402479e98ef97bd
Dristi123jeet <154901301+Dristi123jeet@users.noreply.github.com>
2025-06-26 19:05:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check if two strings are same or not/check-if-two-strings-are-same-or-not.cpp
+++ Difficulty: Easy/Check if two strings are same or not/check-if-two-strings-are-same-or-not.cpp
@@ -1,16 +1,9 @@
 // User function Template for C++
 bool areStringsSame(string s1, string s2) {
     // code here
-     if (s1.length() != s2.length()) {
-        return false;
+    if(s1.size()!=s2.size())return false;
+    for(int i ; i<s1.size(); i++){
+        if(s1[i]!= s2[i])return false;
     }
-
-    // Compare character by character
-    for (size_t i = 0; i < s1.length(); ++i) {
-        if (s1[i] != s2[i]) {
-            return false; 
-        }
-    }
-
-    return true; 
+    return true;
 }
